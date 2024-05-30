9d0b90197be94a5363392794d21270277b3ea1d9
moneyhoardermike <moneyhoardersglobal@gmail.com>
2024-05-30 14:22:57 -0400

    update the set greeting button it is now working

--- packages/nextjs/app/bop-ui/_components/ContractInteraction.tsx
+++ packages/nextjs/app/bop-ui/_components/ContractInteraction.tsx
@@ -6,17 +6,8 @@ import { TransactionReceipt } from "viem";
 
 export const ContractInteraction = () => {
   const [visible, setVisible] = useState(true);
-  const [newGreeting, setNewGreeting] = useState("");
-
-  const { writeAsync, isLoading } = useScaffoldWriteContract<YourContract>({
-    contractName: "YourContract",
-    functionName: "setGreeting",
-    args: [newGreeting],
-    value: "0.01",
-    onBlockConfirmation: (txnReceipt: TransactionReceipt) => {
-      console.log("📦 Transaction blockHash", txnReceipt.blockHash);
-    },
-  });
+  const [newGreeting, setNewGreeting] = useState<string | undefined>(undefined);
+  const { writeContractAsync } = useScaffoldWriteContract("YourContract");
 
   return (
     <div className="flex bg-[url('/assets/background.jpeg')] relative pb-10">
@@ -58,19 +49,20 @@ export const ContractInteraction = () => {
             />
             <div className="flex rounded-full border border-primary p-1 flex-shrink-0">
               <div className="flex rounded-full border-2 border-primary p-1">
-                <button
-                  className="btn btn-primary rounded-full capitalize font-normal font-white w-24 flex items-center gap-1 hover:gap-2 transition-all tracking-widest"
-                  onClick={() => writeAsync()}
-                  disabled={isLoading}
-                >
-                  {isLoading ? (
-                    <span className="loading loading-spinner loading-sm"></span>
-                  ) : (
-                    <>
-                      Send <ArrowSmallRightIcon className="w-3 h-3 mt-0.5" />
-                    </>
-                  )}
-                </button>
+              <button
+              className="btn btn-primary uppercase"
+                  onClick={async () => {
+                    try {
+                      {
+                        await writeContractAsync({ functionName: "setGreeting", args: [newGreeting] });
+                      }
+                    } catch (err) {
+                      console.error("Error calling execute function");
+                    }
+                  }}
+            >
+              Send!
+            </button>
               </div>
             </div>
           </div>
